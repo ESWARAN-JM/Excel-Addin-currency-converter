be1f1a78a6cab0a26ba3b1bee0798b5e2c0e9648
ESWARAN JM <eswaran22032003@gmail.com>
2026-07-14 15:47:50 +0530

    backend fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => {
+app.get(["/", "/api"], (req, res) => {
   res.send("Backend running successfully");
 });
 app.use("/api/auth", authRoutes);
